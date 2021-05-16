46715ff88dd9d749243897f40a3eae0cf77d0bfe
bourn404 <bourn404@gmail.com>
2021-05-15 23:21:29 -0600

    Added dropdown component.  Documents page loads hardcoded docs.

--- src/app/documents/document-list/document-list.component.ts
+++ src/app/documents/document-list/document-list.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Document } from '../document.model';
 
 @Component({
   selector: 'app-document-list',
@@ -7,9 +8,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DocumentListComponent implements OnInit {
 
+  @Output() documentWasSelected = new EventEmitter<Document>();
+
+  documents: Document[] = [
+    new Document(1,'My First Document','The OG document with OG content', 'https://www.carsonfairbourn.com',[]),
+    new Document(2,'My Second Document','Not the OG document with OG content', 'https://www.carsonfairbourn.com',[]),
+    new Document(3,'My Third Document','Could be better. Could be worse.', 'https://www.carsonfairbourn.com',[]),
+    new Document(4,'My Fourth Document','And another one...', 'https://www.carsonfairbourn.com',[]),
+    new Document(5,'The Last Document','A great conclusion to the series.', 'https://www.carsonfairbourn.com',[])
+  ]
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onDocumentSelected(document: Document) {
+    this.documentWasSelected.emit(document);
   }
 
 }
